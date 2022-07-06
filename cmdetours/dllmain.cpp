e6e0820df5ab5ce8141fac91a97b0bf0d530d3e7
Zakhar Afonin <zakharafoniam@gmail.com>
2022-07-06 21:05:30 +0300

    Laying out for Detours

--- cmdetours/dllmain.cpp
+++ cmdetours/dllmain.cpp
@@ -1,17 +1,28 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <detours/detours.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
+    if (DetourIsHelperProcess())
+        return TRUE;
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+        DetourRestoreAfterWith();
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        DetourTransactionCommit();
+        break;
     case DLL_PROCESS_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        DetourTransactionCommit();
         break;
     }
     return TRUE;
